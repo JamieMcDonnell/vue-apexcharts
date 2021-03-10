cbc19f8c05ab24522cf4215f7d40ff473d293e8e
JamieMcDonnell <jamie.mcdonnell@gmail.com>
2021-03-10 09:18:37 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-import ApexCharts from "apexcharts/dist/apexcharts.min";
-import ApexChartsComponent from './ApexCharts.component';
 /* eslint-disable */
 import Vue3Apexcharts from "./vue3-apexcharts";
 
 debugger
-/*
+
 const fill = window.SVG.Gradient.prototype.fill;
 console.log('FILL: ', fill)
-
+/*
 window.Gradient.prototype.fill = function (...args) {
   const url = fill.apply(this, args);
   const prefix = `url(${document.location.href}`;
